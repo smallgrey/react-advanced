e8f9ab596e0c6707253c850aa5280f542f8706b3
smallgrey <15234109892@163.com>
2021-04-05 23:56:42 +0800

    react脚手架模板项目搭建基本页面及路由配置

--- react-quick-start/src/index.js
+++ react-quick-start/src/index.js
@@ -3,15 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import { mainRoute } from './routes';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Switch>
+      <Route path="/admin" component={App}/>
+      {
+        mainRoute.map( (route, key)=>{
+          return <Route path={route.pathname} component={route.component} key={key}/>
+        })
+      }
+      <Redirect to='/home' from='/' exact/>
+    </Switch>
+  </Router>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
